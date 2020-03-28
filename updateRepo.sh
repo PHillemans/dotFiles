53e240d33d8bc0a4d4e881427581d2ee5b941138
Pepijn Hillemans <pepino@pop-os.localdomain>
2020-03-28 17:01:17 +0100

    changed scope

---
+++ updateRepo.sh
@@ -0,0 +1,7 @@
+cp ~/.config/i3 ./.config/
+cp ~/.config/rofi ./.config
+cp ~/.config/polybar ./.config
+cp ~/.config/powermenu ./.config
+cp ~/.config/nvim ./.config
+cp ~/.config/kitty/kitty.conf ./.config
+
